4db3f43379aeca8da06b305e91b617ea9e947a17
mahuikai <675398040@qq.com>
2018-06-04 10:39:44 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,482 @@
+
+	data1 = [
+		[{
+				name: '已恢复',
+				value: 146,
+				itemStyle: {
+					normal: {
+						color: 'orange',
+						label: {
+							show: true,
+							position: 'center'
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			},
+			{
+				name: '未恢复',
+				value: 58,
+				itemStyle: {
+					normal: {
+						color: '#4581d9',
+						label: {
+							show: true,
+							position: 'center',
+							textStyle: {
+								baseline: 'bottom'
+							}
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			}
+		],
+
+		[{
+				name: '已恢复',
+				value: 865130,
+				itemStyle: {
+					normal: {
+						color: 'orange',
+						label: {
+							show: true,
+							position: 'center'
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			},
+			{
+				name: '未恢复',
+				value: 1654879,
+				itemStyle: {
+					normal: {
+						color: '#4581d9',
+						label: {
+							show: true,
+							position: 'center',
+							textStyle: {
+								baseline: 'bottom'
+							}
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			}
+		],
+
+		[{
+				name: '已恢复',
+				value: 775648,
+				itemStyle: {
+					normal: {
+						color: 'orange',
+						label: {
+							show: true,
+							position: 'center'
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			},
+			{
+				name: '未恢复',
+				value: 1964583,
+				itemStyle: {
+					normal: {
+						color: '#4581d9',
+						label: {
+							show: true,
+							position: 'center',
+							textStyle: {
+								baseline: 'bottom'
+							}
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			}
+		],
+
+		[{
+				name: '已恢复',
+				value: 563215,
+				itemStyle: {
+					normal: {
+						color: 'orange',
+						label: {
+							show: true,
+							position: 'center'
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			},
+			{
+				name: '未恢复',
+				value: 2665483,
+				itemStyle: {
+					normal: {
+						color: '#4581d9',
+						label: {
+							show: true,
+							position: 'center',
+							textStyle: {
+								baseline: 'bottom'
+							}
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			}
+		],
+
+		[{
+				name: '已恢复',
+				value: 462459,
+				itemStyle: {
+					normal: {
+						color: 'orange',
+						label: {
+							show: true,
+							position: 'center'
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			},
+			{
+				name: '未恢复',
+				value: 2635684,
+				itemStyle: {
+					normal: {
+						color: '#4581d9',
+						label: {
+							show: true,
+							position: 'center',
+							textStyle: {
+								baseline: 'bottom'
+							}
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			}
+		],
+
+		[{
+				name: '已恢复',
+				value: 862135,
+				itemStyle: {
+					normal: {
+						color: 'orange',
+						label: {
+							show: true,
+							position: 'center'
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			},
+			{
+				name: '未恢复',
+				value: 2405648,
+				itemStyle: {
+					normal: {
+						color: '#4581d9',
+						label: {
+							show: true,
+							position: 'center',
+							textStyle: {
+								baseline: 'bottom'
+							}
+						},
+						labelLine: {
+							show: false
+						}
+					}
+				}
+			}
+		],
+	];
+	//		左下饼状图数据
+	data31 = [
+		[{
+			value: 0,
+			name: '2G'
+		}, {
+			value: 770,
+			name: '3G'
+		}, {
+			value: 3301,
+			name: '4G'
+		}],
+		[{
+			value: 0,
+			name: '2G'
+		}, {
+			value: 987,
+			name: '3G'
+		}, {
+			value: 0,
+			name: '4G'
+		}],
+		[{
+			value: 756,
+			name: '2G'
+		}, {
+			value: 562,
+			name: '3G'
+		}, {
+			value: 124,
+			name: '4G'
+		}],
+		[{
+			value: 354,
+			name: '2G'
+		}, {
+			value: 354,
+			name: '3G'
+		}, {
+			value: 685,
+			name: '4G'
+		}],
+		[{
+			value: 759,
+			name: '2G'
+		}, {
+			value: 215,
+			name: '3G'
+		}, {
+			value: 456,
+			name: '4G'
+		}],
+		[{
+			value: 215,
+			name: '2G'
+		}, {
+			value: 423,
+			name: '3G'
+		}, {
+			value: 432,
+			name: '4G'
+		}],
+		[{
+			value: 642,
+			name: '2G'
+		}, {
+			value: 354,
+			name: '3G'
+		}, {
+			value: 685,
+			name: '4G'
+		}],
+		[{
+			value: 439,
+			name: '2G'
+		}, {
+			value: 345,
+			name: '3G'
+		}, {
+			value: 456,
+			name: '4G'
+		}],
+		[{
+			value: 552,
+			name: '2G'
+		}, {
+			value: 634,
+			name: '3G'
+		}, {
+			value: 432,
+			name: '4G'
+		}],
+		[{
+			value: 345,
+			name: '2G'
+		}, {
+			value: 124,
+			name: '3G'
+		}, {
+			value: 228,
+			name: '4G'
+		}]
+	];
+	data32 = [
+		[{
+			value: 84.87,
+			name: '标清'
+		}, {
+			value: 1.05,
+			name: '高清'
+		}, {
+			value: 0,
+			name: '4K'
+		}],
+		[{
+			value: 46.2,
+			name: '标清'
+		}, {
+			value: 39.1,
+			name: '高清'
+		}, {
+			value: 14.7,
+			name: '4K'
+		}],
+		[{
+			value: 42.8,
+			name: '标清'
+		}, {
+			value: 35.4,
+			name: '高清'
+		}, {
+			value: 21.8,
+			name: '4K'
+		}],
+		[{
+			value: 41.3,
+			name: '标清'
+		}, {
+			value: 38.2,
+			name: '高清'
+		}, {
+			value: 21.5,
+			name: '4K'
+		}],
+		[{
+			value: 48.2,
+			name: '标清'
+		}, {
+			value: 34.1,
+			name: '高清'
+		}, {
+			value: 17.7,
+			name: '4K'
+		}],
+		[{
+			value: 46.5,
+			name: '标清'
+		}, {
+			value: 32.7,
+			name: '高清'
+		}, {
+			value: 20.8,
+			name: '4K'
+		}],
+		[{
+			value: 49.2,
+			name: '标清'
+		}, {
+			value: 25.4,
+			name: '高清'
+		}, {
+			value: 25.4,
+			name: '4K'
+		}],
+		[{
+			value: 47.6,
+			name: '标清'
+		}, {
+			value: 34.5,
+			name: '高清'
+		}, {
+			value: 17.9,
+			name: '4K'
+		}],
+		[{
+			value: 44.3,
+			name: '标清'
+		}, {
+			value: 32.2,
+			name: '高清'
+		}, {
+			value: 23.5,
+			name: '4K'
+		}],
+		[{
+			value: 43.1,
+			name: '标清'
+		}, {
+			value: 31.5,
+			name: '高清'
+		}, {
+			value: 25.5,
+			name: '4K'
+		}]
+	]
+
+	function getNowFormatDate() {
+		var date = new Date();
+		var seperator1 = "-";
+		var seperator2 = ":";
+		var month = date.getMonth() + 1;
+		var strDate = date.getDate();
+		var hour = date.getHours();
+		var minite = date.getMinutes();
+		var second = date.getSeconds();
+		if(month >= 1 && month <= 9) {
+			month = "0" + month;
+		}
+		if(strDate >= 0 && strDate <= 9) {
+			strDate = "0" + strDate;
+		}
+		if(hour >= 0 && hour <= 9) {
+			hour = "0" + hour;
+		}
+		if(minite >= 0 && minite <= 9) {
+			minite = "0" + minite;
+		}
+		if(second >= 0 && second <= 9) {
+			second = "0" + second;
+		}
+
+		var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
+			" " + hour + seperator2 + minite +
+			seperator2 + second;
+		return currentdate;
+	}
+	//		右柱状图数据
+	data4 = data5 = [
+		[326381, 359744, 386778, 403904, 416767, 435864, 604275, 728756, 796914, 967974],
+		[86772, 66836, 252, 403904, 0, 67529, 29085, 88672, 71676, 126714],
+		[1869, 2681, 3265, 3847, 4060, 4123, 5866, 6778, 14014, 24465],
+		[2348, 2765, 3659, 7765, 8125, 9030, 10497, 12315, 14653, 24765],
+		[3685, 4103, 4356, 6758, 9348, 10354, 12398, 13174, 21325, 967974],
+		[3100, 3468, 4685, 8651, 12055, 14653, 16352, 18564, 29654, 32983],
+		[3100, 4897, 6753, 7246, 9543, 10965, 11065, 13174, 19654, 24658],
+	];
+
+	data41 = data51 = [
+		['湖北', '辽宁', '湖南', '安徽', '江苏', '浙江', '四川', '山东', '河南', '广东'],
+		['音乐频道(直播)', 'CCTV-7(直播)', 'CCTV-6(直播)', 'CCTV-5(直播)', '熊出没(点播)', 'CCTV-2(直播)', 'CCTV-3(直播)', 'CCTV-1(直播)', '住宅', '学校校园'],
+		['其他', '商业购物区', '政企单位', '城市干路', '工业园区', '交通干线', '行政村', '乡镇', '住宅', '学校校园'],
+		['CCTV-6(直播)', 'CCTV-2(直播)', '音乐频道(直播)', 'CCTV-5(直播)', '熊出没(点播)', 'CCTV-1(直播)', 'CCTV-3(直播)', '综艺频道(直播)', 'CCTV-7(直播)', 'CCTV-4(直播)'],
+		['战狼2(点播)', 'CCTV-7(直播)', 'CCTV-6(直播)', 'CCTV-5(直播)', '综艺频道(直播)', 'CCTV-4(直播)', 'CCTV-3(直播)', 'CCTV-1(直播)', '熊出没(点播)', 'CCTV-2(直播)'],
+		['CCTV-5(直播)', 'CCTV-7(直播)', '战狼2(点播)', 'CCTV-6(直播)', 'CCTV-2(直播)', 'CCTV-4(直播)', 'CCTV-1(直播)', '熊出没(点播)', '综艺频道(直播)', 'CCTV-3(直播)'],
+		['战狼2(点播)', 'CCTV-7(直播)', 'CCTV-6(直播)', 'CCTV-4(直播)', 'CCTV-5(直播)', '综艺频道(直播)', 'CCTV-3(直播)', 'CCTV-2(直播)', 'CCTV-1(直播)', '熊出没(点播)'],
+
+	]
+
